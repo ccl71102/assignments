27f6ceec683fc155af2c01a0f061ee938612efab
ccl71102 <colonlopezcarlos@gmail.com>
2019-07-25 17:29:45 -0600

    fixed scrolling issue on portfolio site

--- projects/personal-portfolio/src/App.js
+++ projects/personal-portfolio/src/App.js
@@ -3,18 +3,21 @@ import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js";
-import Contact from "./components/Contact.js";
-import { Switch, Route} from "react-router-dom";
+import { Switch, Route, withRouter} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.js"
+
+const Scroll = withRouter(ScrollToTop);
 
 const App = () => {
   
   return    <div>
               <Navbar/>
-              <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/projects" component={Projects}/>
-                <Route path="/contact" component={Contact}/>
-              </Switch>
+              <Scroll>
+                <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/projects" component={Projects}/>
+                </Switch>
+              </Scroll>
               <Footer/>
             </div>
 
