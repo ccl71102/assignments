2b8fd05e301adfcae8572406f03c79eb1176a102
ccl71102 <colonlopezcarlos@gmail.com>
2019-08-09 17:32:28 -0600

    added more spacing between project listings

--- exercises/one-on-one/main.js
+++ exercises/one-on-one/main.js
@@ -22,46 +22,75 @@
 // console.log(twoSum([1, 2, 3], 4));
 // console.log(twoSum([5, 2, 8, 40, 5, 12, 6], 20));
 
-const get12HourFormat = d => {
-    if(d.getHours() > 12)
-        return d.getHours() - 12;
-    else if (d.getHours === 0)
-        return 12;
-    else
-        return d.getHours();
+// const get12HourFormat = d => {
+//     if(d.getHours() > 12)
+//         return d.getHours() - 12;
+//     else if (d.getHours === 0)
+//         return 12;
+//     else
+//         return d.getHours();
+// }
+
+// const getMonth = d => {
+//     const months = [ "January", 
+//                         "February", 
+//                         "March", 
+//                         "April",
+//                         "May",
+//                         "June",
+//                         "July",
+//                         "August",
+//                         "September",
+//                         "October",
+//                         "November",
+//                         "December"];
+//     return months[d.getMonth()];
+// }
+
+// const getDate = () => {
+
+//     let d = new Date();
+//     d.setMinutes(d.getMinutes() + 30);
+//     let month = getMonth(d);
+//     let day = d.getDate();
+//     let suffix = d.getHours() >= 12 ? 'PM' : "AM";
+//     let hour = get12HourFormat(d);
+//     let minutes = d.getMinutes();
+
+//     return `Your order has been placed and should be ready on ${month} ${day} by ${hour}:${minutes} ${suffix}.`;
+// }
+
+// getDate();
+
+
+//Game of Threes
+    // Write a recursive function that takes two integers, 
+    // n and counter as parameters and does the following:
+
+    // If the n is divisible by three, divide it by three and call the 
+    // function again, providing the new value as n and counter + 1 as counter
+
+    // If not, either add or subtract 1 to a get a new value that 
+    // is divisible by three and repeat step 1.
+
+    // If n is one, simply return the value counter.
+
+function gameOfThrees(n, counter){
+    
+    if(n === 1)
+        return counter;
+
+    if(n%3 === 0){
+        return gameOfThrees((n/3), (counter + 1))
+    } 
+    
+    else {
+        return gameOfThrees((n-1), counter)
+    }
 }
 
-const getMonth = d => {
-    const months = [ "January", 
-                        "February", 
-                        "March", 
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                        "August",
-                        "September",
-                        "October",
-                        "November",
-                        "December"];
-    return months[d.getMonth()];
-}
-
-const getDate = () => {
-
-    let d = new Date();
-    d.setMinutes(d.getMinutes() + 30);
-    let month = getMonth(d);
-    let day = d.getDate();
-    let suffix = d.getHours() >= 12 ? 'PM' : "AM";
-    let hour = get12HourFormat(d);
-    let minutes = d.getMinutes();
-
-    return `Your order has been placed and should be ready on ${month} ${day} by ${hour}:${minutes} ${suffix}.`;
-}
-
-getDate();
-
+console.log(gameOfThrees(10000, 0))
+// Output:  8
 
 
 
